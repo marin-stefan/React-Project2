8b11a3010314f5ee899efcc0211949f1a3df28ea
stefann06 <stefann06@yahoo.com>
2021-12-10 13:43:35 +0200

    gitignore update

---
+++ src/configs/firebase.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDpi1OGKFC4ENAmDzFs0oRAP4nuCByIppY",
+    authDomain: "the-90-s-1a628.firebaseapp.com",
+    projectId: "the-90-s-1a628",
+    storageBucket: "the-90-s-1a628.appspot.com",
+    messagingSenderId: "596641414089",
+    appId: "1:596641414089:web:5c20b75210bb42e44f8618"
+  };
+export default firebaseConfig;
