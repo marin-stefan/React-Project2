cfb3e7819fab8087ed46563c0dcdb15f17a2980a
stefann06 <stefann06@yahoo.com>
2024-09-13 16:28:27 +0300

    Updates

---
+++ precache-manifest.d902f207a28090007db11ed88fdb20f4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119356dbbba50dfa89090730b05ed4f2",
+    "url": "/ReactProject2-ECommerce/index.html"
+  },
+  {
+    "revision": "212e0f3c435e3bcbbf06",
+    "url": "/ReactProject2-ECommerce/static/css/2.5f10bdda.chunk.css"
+  },
+  {
+    "revision": "1c9613f12a1567b00d1f",
+    "url": "/ReactProject2-ECommerce/static/css/main.ce923acd.chunk.css"
+  },
+  {
+    "revision": "212e0f3c435e3bcbbf06",
+    "url": "/ReactProject2-ECommerce/static/js/2.a3f80b4e.chunk.js"
+  },
+  {
+    "revision": "1c9613f12a1567b00d1f",
+    "url": "/ReactProject2-ECommerce/static/js/main.f0edfbda.chunk.js"
+  },
+  {
+    "revision": "a4677f29446dc8f2d1c4",
+    "url": "/ReactProject2-ECommerce/static/js/runtime-main.64cb9a78.js"
+  },
+  {
+    "revision": "1d3902bd6cf2b0af808bbfdf51dec415",
+    "url": "/ReactProject2-ECommerce/static/media/favorite_black.1d3902bd.svg"
+  },
+  {
+    "revision": "aa9856d374500085fae2996df19a4568",
+    "url": "/ReactProject2-ECommerce/static/media/logo3.aa9856d3.png"
+  },
+  {
+    "revision": "4eb6a9305e0b9b55be802ffa8bfc56c3",
+    "url": "/ReactProject2-ECommerce/static/media/shopping-cart.4eb6a930.svg"
+  }
+]);
